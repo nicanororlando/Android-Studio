642b3eabde9355e7288f0ea46e5c96c9ee9435c1
nicanororlando <nicanororlando@gmail.com>
2021-12-22 10:40:47 -0300

    Termino el TabView y todo lo de fragments

--- TareaSemana4/app/src/main/java/example/com/tareasemana4/BaseDatos/Presentador/IRecyclerViewFragmentPresenter.java
+++
@@ -1,7 +0,0 @@
-package example.com.tareasemana4.BaseDatos.Presentador;
-
-public interface IRecyclerViewFragmentPresenter {
-
-    void obtenerMascotasBaseDatos();
-    void mostrarMascotasRV();
-}
--- TareaSemana4/app/src/main/java/example/com/tareasemana4/DetallesMascota/BackKeyPressedListener.java
+++
@@ -1,7 +0,0 @@
-package example.com.tareasemana4.DetallesMascota;
-
-import androidx.viewpager2.widget.ViewPager2;
-
-public interface BackKeyPressedListener{
-    void onBackPressed(ViewPager2 viewPager2);
-}
